4fc7822de7581c394669a7705d66b26ddc530a0b
Sergi-Guiral-Solsona <guiral.solsona@gmail.com>
2021-03-26 10:48:19 +0100

    i dont says hello

--- Git_test/Program.cs
+++ Git_test/Program.cs
@@ -6,7 +6,7 @@ namespace Git_test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("I dont gonna says hello");
         }
     }
 }
